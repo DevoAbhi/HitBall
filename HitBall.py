e5ce583779add43d0d820dca7f43e2d2d30ee3bd
AbhiDroid <abhinabroy2001@gmail.com>
2021-02-02 21:05:52 +0530

    Add paddles on both the sides and some styling changes

--- HitBall.py
+++ HitBall.py
@@ -8,7 +8,7 @@ win = turtle.Screen()
 win.title("HitBall by DevoAbhi!")
 
 # Adding background color
-win.bgcolor("black")
+win.bgcolor("green")
 
 # Giving a basic widht and height to the screen
 win.setup(width=800, height=600)
@@ -16,5 +16,42 @@ win.setup(width=800, height=600)
 # This is added so that there is no update in the screen. This enables faster and smoother gameplay
 win.tracer(0) 
 
+# Paddle A graphics
+paddle_a = turtle.Turtle()
+
+#This is speed of animation. 0 is the fastest upto 10
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color("brown")
+
+# This makes sure that the object created does not draw 
+# anyting on the window
+paddle_a.penup()
+
+# Give shape to the object - Our basic object has a shape of 20px-20px, so stretch-wid=5 means 5 times the default
+paddle_a.shapesize(stretch_wid=7, stretch_len=1)
+
+# Position of the object 
+paddle_a.goto(-365, 0)
+
+# Paddle B graphics
+paddle_b = turtle.Turtle()
+
+#This is speed of animation. 0 is the fastest upto 10
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color("brown")
+
+# This makes sure that the object created does not draw 
+# anyting on the window
+paddle_b.penup()
+
+# Give shape to the object - Our basic object has a shape of 20px-20px, so stretch-wid=5 means 5 times the default
+paddle_b.shapesize(stretch_wid=7, stretch_len=1)
+
+# Position of the object 
+paddle_b.goto(365, 0)
+
+# Main game loop
 while True:
     win.update()
